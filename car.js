3d74d50dc0201a9484711c669f3f906df8544a33
ayushsrinivas7 <ayushsrinivas7@gmail.com>
2023-10-07 15:55:46 +0530

    the collision detection is deteced

--- car.js
+++ car.js
@@ -4,7 +4,8 @@ class Car{
         this.y=y;
         this.width=width;
         this.height=height;
-       
+        
+        this.damaged = false;
         this.speed=0;
         this.acceleration=0.2;
         this.maxSpeed=3;
@@ -15,8 +16,56 @@ class Car{
     }
 
     update(roadBorders ){
+        // this.#move();
+        // this.points = this.#createpolygon();
+        // this.damage = this.#assessDamage(roadBorders);
+
+        if( ! this.damaged ){
         this.#move();
+        this.polygon = this.#createpolygon();
+        this.damaged = this.#assessDamage(roadBorders);
+        }
+
         this.sensors.update( roadBorders );
+    }
+
+    #assessDamage(roadBorders){
+        for( let i = 0 ; i< roadBorders.length ; i++ ){
+          if( polysIntersect (this.polygon , roadBorders[i])){
+            return true;
+          }  
+          
+        }
+
+        return false;
+    }
+
+    #createpolygon(){
+        const points = [];
+        const rad = Math.hypot( this.width , this.height )/2;
+        const alpha = Math.atan2 (this.width , this.height );
+
+        points.push( { 
+            x : this.x - ( Math.sin(this.angle - alpha ) * rad ) ,
+            y : this.y -  ( Math.cos(this.angle - alpha ) * rad )
+        })
+
+        points.push( { 
+            x : this.x - ( Math.sin(this.angle + alpha ) * rad ) ,
+            y : this.y -  ( Math.cos(this.angle + alpha ) * rad )
+        })
+
+        points.push( { 
+            x : this.x - ( Math.sin( Math.PI + this.angle - alpha ) * rad ) ,
+            y : this.y -  ( Math.cos( Math.PI + this.angle - alpha ) * rad )
+        })
+
+        points.push( { 
+            x : this.x - ( Math.sin( Math.PI + this.angle + alpha ) * rad ) ,
+            y : this.y -  ( Math.cos( Math.PI + this.angle + alpha ) * rad )
+        })
+         
+        return points;
     }
 
     #move(){
@@ -59,17 +108,22 @@ class Car{
     }
 
     draw(ctx){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
+        if( this.damaged ){
+            ctx.fillStyle = "gray";
+        }else{
+            ctx.fillStyle = "black";
+        }
 
+
+        ctx.save();
+       
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
+        ctx.moveTo ( this.polygon[0].x , this.polygon[0].y );
+        for( let i = 1 ; i< this.polygon.length ; i++ ){
+         ctx.lineTo( this.polygon[i].x , this.polygon[i].y );
+         ctx.stroke();
+        }
+        
         ctx.fill();
         
         ctx.restore();
